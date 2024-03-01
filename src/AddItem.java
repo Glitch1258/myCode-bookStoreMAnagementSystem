d2591977878ef7b5800730b18e42a016f44c3fd7
Glitch12358 <glitch12358@gmail.com>
2024-03-02 03:59:30 +0600

    proper order from this point . Good template to sttart from

--- src/AddItem.java
+++ src/AddItem.java
@@ -3,12 +3,19 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class AddItem extends JFrame implements ActionListener {
-    JButton view, add, update, remove;
-
+    JLabel container , heading , bookTitleLabel ,
+            numberOfPagesLabel , bookGenreLabel , authorNameLabel ,
+            costPriceLabel,sellingPriceLabel,bookDescriptionLabel;
+    JTextField bookTitleTextField,numberOfPagesTextField,
+            bookGenreTextField,authorNameTextField,
+            costPriceTextField,sellingPriceTextField;
+    JTextArea bookDescriptionTextArea;
+    JScrollPane scrollPane;
+    JButton addToInventoryButton;
     AddItem() {
         setLayout(null);
 
-        JLabel container = new JLabel() {
+        container = new JLabel() {
             // Override paintComponent to set background color
             @Override
             protected void paintComponent(Graphics g) {
@@ -21,71 +28,71 @@ public class AddItem extends JFrame implements ActionListener {
         container.setBackground(Color.darkGray);
         add(container);
 
-        JLabel heading = new JLabel("Add Book");
+        heading = new JLabel("Add Book");
         heading.setBounds(20, 20, 400, 40);
         heading.setFont(new Font("Raleway", Font.BOLD, 25));
         container.add(heading);
 
-        JLabel bookTitleLabel = new JLabel("Book Title : ");
+        bookTitleLabel = new JLabel("Book Title : ");
         bookTitleLabel.setBounds(20,50,1000,100);
         container.add(bookTitleLabel);
 
-        JTextField bookTitleTextField = new JTextField();
+        bookTitleTextField = new JTextField();
         bookTitleTextField.setBounds(150, 85, 350, 30);
         container.add(bookTitleTextField);
 
-        JLabel numberOfPagesLabel = new JLabel("Number of pages : ");
+        numberOfPagesLabel = new JLabel("Number of pages : ");
         numberOfPagesLabel.setBounds(20,100,1000,100);
         container.add(numberOfPagesLabel);
 
-        JTextField numberOfPagesTextField = new JTextField();
+        numberOfPagesTextField = new JTextField();
         numberOfPagesTextField.setBounds(150, 135, 350, 30);
         container.add(numberOfPagesTextField);
 
-        JLabel bookGenreLabel = new JLabel("Book Genre : ");
+        bookGenreLabel = new JLabel("Book Genre : ");
         bookGenreLabel.setBounds(20,150,1000,100);
         container.add(bookGenreLabel);
 
-        JTextField bookGenreTextField = new JTextField();
+        bookGenreTextField = new JTextField();
         bookGenreTextField.setBounds(150, 185, 350, 30);
         container.add(bookGenreTextField);
 
-        JLabel authorNameLabel = new JLabel("Author's Name : ");
+        authorNameLabel = new JLabel("Author's Name : ");
         authorNameLabel.setBounds(20,200,1000,100);
         container.add(authorNameLabel);
 
-        JTextField authorNameTextField = new JTextField();
+        authorNameTextField = new JTextField();
         authorNameTextField.setBounds(150, 235, 350, 30);
         container.add(authorNameTextField);
 
-        JLabel costPriceLabel = new JLabel("Cost Price : ");
+        costPriceLabel = new JLabel("Cost Price : ");
         costPriceLabel.setBounds(20,250,1000,100);
         container.add(costPriceLabel);
 
-        JTextField costPriceTextField = new JTextField();
+        costPriceTextField = new JTextField();
         costPriceTextField.setBounds(150, 285, 350, 30);
         container.add(costPriceTextField);
 
-        JLabel sellingPriceLabel = new JLabel("Selling Price : ");
+        sellingPriceLabel = new JLabel("Selling Price : ");
         sellingPriceLabel.setBounds(20,300,1000,100);
         container.add(sellingPriceLabel);
 
-        JTextField sellingPriceTextField = new JTextField();
+        sellingPriceTextField = new JTextField();
         sellingPriceTextField.setBounds(150, 335, 350, 30);
         container.add(sellingPriceTextField);
 
-        JLabel bookDescriptionLabel = new JLabel("Book Description : ");
+        bookDescriptionLabel = new JLabel("Book Description : ");
         bookDescriptionLabel.setBounds(20,350,1000,100);
         container.add(bookDescriptionLabel);
 
-        JTextArea bookDescriptionTextArea = new JTextArea();
+        bookDescriptionTextArea = new JTextArea();
         bookDescriptionTextArea.setLineWrap(true); // Enable word wrap
         bookDescriptionTextArea.setWrapStyleWord(true); // Wrap at word boundaries
-        JScrollPane scrollPane = new JScrollPane(bookDescriptionTextArea);
+        scrollPane = new JScrollPane(bookDescriptionTextArea);
         scrollPane.setBounds(150, 385, 300, 100); // Adjust as needed
         container.add(scrollPane);
 
-        JButton addToInventoryButton = new JButton("Add Item To Inventory");
+        addToInventoryButton = new JButton("Add Item To Inventory");
         addToInventoryButton.setBounds(150, 500, 200, 30);
         addToInventoryButton.setFocusable(false);
         addToInventoryButton.addActionListener(this);
@@ -101,7 +108,13 @@ public class AddItem extends JFrame implements ActionListener {
 
     public void actionPerformed(ActionEvent actionEvent) {
         if (actionEvent.getActionCommand().equals("Add Item To Inventory")) {
-            System.out.println("Button clicked!");
+
+            String title = bookTitleTextField.getText();
+            String genre = bookGenreTextField.getText();
+            String author = authorNameTextField.getText();
+            System.out.println("title " + title);
+            System.out.println("genre " + genre);
+            System.out.println("author " + author);
         }
     }
 
