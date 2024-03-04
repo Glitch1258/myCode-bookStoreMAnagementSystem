5e2418bc8527b1c5a223b34f2094b6cb915aa23b
Glitch12358 <glitch12358@gmail.com>
2024-03-04 14:25:49 +0600

    proper order from this point . Good template to sttart from

---
+++ src/LoginPage.java
@@ -0,0 +1,92 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+
+public class LoginPage extends JFrame implements ActionListener {
+    DatabaseHandler databaseHandler = new DatabaseHandler("bookStore");
+    Connection databaseConnection = databaseHandler.getDatabaseConnection();
+
+
+    JLabel container, heading, userNameLabel, passwordLabel;
+    JTextField userNameTextField, passwordTextField;
+
+    JButton logInButton;
+
+
+    LoginPage() {
+        setLayout(null);
+
+        container = new JLabel() {
+            // Override paintComponent to set background color
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.lightGray);
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        };
+        container.setBounds(0, 0, 1000, 400);
+        container.setBackground(Color.darkGray);
+        add(container);
+
+        heading = new JLabel("Login");
+        heading.setBounds(20, 20, 400, 40);
+        heading.setFont(new Font("Raleway", Font.BOLD, 25));
+        container.add(heading);
+
+
+        userNameLabel = new JLabel("User Name : ");
+        userNameLabel.setBounds(20, 50, 1000, 100);
+        container.add(userNameLabel);
+
+        userNameTextField = new JTextField();
+        userNameTextField.setBounds(150, 85, 350, 30);
+        container.add(userNameTextField);
+
+        passwordLabel = new JLabel("Password : ");
+        passwordLabel.setBounds(20, 100, 1000, 100);
+        container.add(passwordLabel);
+
+        passwordTextField = new JTextField();
+        passwordTextField.setBounds(150, 135, 350, 30);
+        container.add(passwordTextField);
+
+
+        logInButton = new JButton("log in");
+        logInButton.setBounds(150, 250, 200, 30);
+        logInButton.setFocusable(false);
+        logInButton.addActionListener(this);
+        logInButton.setFont(new Font("Arial", Font.ITALIC, 14));
+        // addToInventoryButton.setIcon(new ImageIcon("F:\\projects\\"));
+        container.add(logInButton);
+
+
+        setSize(1000, 400);
+        setLocation(250, 100);
+        setVisible(true);
+        setResizable(false);
+    }
+
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == logInButton) {
+            String userName = userNameTextField.getText();
+            String passWord = passwordTextField.getText();
+            if(userName.isBlank()||passWord.isBlank()){
+                JOptionPane.showMessageDialog(null, "One or both of the input fields is blank!", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            
+
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        new LoginPage();
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,55 @@ public class Main {
     public static void main(String[] args) {
         DatabaseHandler databaseHandler = new DatabaseHandler("bookStore");
 
-        try (Connection conn = databaseHandler.getDatabaseConnection();
-             Statement stmt = conn.createStatement()) {
-
-            ResultSet rs = stmt.executeQuery("SELECT * FROM inventory");
-
-            while (rs.next()) {
-                String id = rs.getString("id");
-                int numberOfPages = rs.getInt("numberOfPages");
-                String title = rs.getString("title");
-                String authorName = rs.getString("authorName");
-                int costPrice = rs.getInt("costPrice");
-                int sellingPrice = rs.getInt("sellingPrice");
-                String description = rs.getString("description");
-                String genre = rs.getString("genre");
-
-                // Reading BLOB data from the coverPageIcon column
-                byte[] coverPageIconBytes = rs.getBytes("coverPageIcon");
-
-                System.out.println("ID: " + id + ", NumberOfPages: " + numberOfPages +
-                        ", Title: " + title + ", AuthorName: " + authorName +
-                        ", CostPrice: " + costPrice + ", SellingPrice: " + sellingPrice +
-                        ", Description: " + description + ", CoverPageIcon size: " + coverPageIconBytes.length+" genre"+genre);
-            }
-
-        } catch (SQLException e) {
-            System.err.println("Error: " + e.getMessage());
+        Connection connection = databaseHandler.getDatabaseConnection();
+        String query = "INSERT INTO admins (userName,password) values (?,?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, "sakib2");
+            preparedStatement.setString(2, "123456");
+            preparedStatement.executeUpdate();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
         }
+
+
+
     }
 }
+//
+//        try (Connection conn = databaseHandler.getDatabaseConnection();
+//             Statement stmt = conn.createStatement()) {
+//
+//            ResultSet rs = stmt.executeQuery("SELECT * FROM inventory");
+//
+//            while (rs.next()) {
+//                String id = rs.getString("id");
+//                int numberOfPages = rs.getInt("numberOfPages");
+//                String title = rs.getString("title");
+//                String authorName = rs.getString("authorName");
+//                int costPrice = rs.getInt("costPrice");
+//                int sellingPrice = rs.getInt("sellingPrice");
+//                String description = rs.getString("description");
+//                String genre = rs.getString("genre");
+//
+//                // Reading BLOB data from the coverPageIcon column
+//                byte[] coverPageIconBytes = rs.getBytes("coverPageIcon");
+//
+//                System.out.println("ID: " + id + ", NumberOfPages: " + numberOfPages +
+//                        ", Title: " + title + ", AuthorName: " + authorName +
+//                        ", CostPrice: " + costPrice + ", SellingPrice: " + sellingPrice +
+//                        ", Description: " + description + ", CoverPageIcon size: " + coverPageIconBytes.length+" genre"+genre);
+//            }
+//
+//        } catch (SQLException e) {
+//            System.err.println("Error: " + e.getMessage());
+//        }
+
+
+
+
+//
+//databaseHandler.runQuery("CREATE TABLE IF NOT EXISTS admins (" +
+//                                 "name TEXT NOT NULL," +
+//                                 "password TEXT NOT NULL" +
+//                                 ")");
